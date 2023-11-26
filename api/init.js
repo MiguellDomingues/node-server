88ad3fcf83494f979ff36a38499ee228c45369c7
Miguel Domingues <42451135+MiguellDomingues@users.noreply.github.com>
2023-11-26 11:57:26 -0800

    clean up, comments, enhanced getStringRoutes to work for any level of nesting

--- api/init.js
+++ api/init.js
@@ -1,36 +1,86 @@
 const _session                        = require('./session.js')
 const routes                         = require('./routes')
 
-//given a path string, traverse the route JSON object 
+/*
+return a list of distinct string routes that resolve to a method on the rootJSON object
+
+params: 
+    routesJSON: the object definition for the endpoint routes. see routes.js
+return:
+    an Array of Strings representing routes consumable by Express.js ie: ["/routeA", "/routeA/routeAA", "/routeB"...]
+*/
+function getStringRoutes(routesJSON){
+
+    /*
+    recursively traverse the routesJSON and generate the string routes (delimited with /) that are consumable by express.js
+    edgecase: each starting route has a //, so I just clean them up with replace("//", "/")
+    params:
+        rootStr: the running name of the route
+        routeObjs: the array of key names for the current object
+    return:
+        all routes from the current level, appending it to the previous levels list  
+    */
+    function f(rootStr, routeObjs){
+
+        const METHODS = [`POST`, `PATCH`, `GET`, `DELETE`, 'PUT'];
+        let addedMethod = false;
+        let routes = [];
+
+        for(routeStr in routeObjs){
+            if(!METHODS.includes(routeStr)){ //if the current level has a nested route, recourse to the next level
+                routes = routes.concat ( f(`${rootStr}/${routeStr}`, routeObjs[routeStr]) );
+            }else if(!addedMethod){ //when we encounter a method, we only need to add it once to the routes
+                routes.push(`${rootStr}`);
+                addedMethod = !addedMethod;
+            }           
+        }
+        return routes;      
+    }
+
+    let stringifiedRoutes = [];
+
+    stringifiedRoutes = f(`/`, routesJSON).map(s=>s.replace("//", "/"));
+
+    console.log("stringifiedRoutes: ", stringifiedRoutes);
+
+    return stringifiedRoutes;
+}
 
 /*
+get a nested object reference from the routes definition JSON
+
 params: 
-    stringifiedRoute: a route string containing 0 or more nested routes (routeA) or (routeB/routeC)
-
-
+    stringifiedRoute: a route string containing 0 or more nested routes (/route or /routeB/routeC)
+    routesJSON: the object definition for the endpoint routes. see routes.js
+return:
+    the nested object reference from rootJSON that can be invoked with object[method][user_type]
 */
 function getRootObject(stringifiedRoute, rootJSON){
+
+    if(stringifiedRoute.length === 0){
+        throw new Error("getRootObject: stringifiedRoute can not be an empty string")
+    }
+
+    stringifiedRoute = stringifiedRoute.slice(1) //REMOVE THE LEADING "/" 
 
     let rootObject = rootJSON
     console.log("stringifiedRoute: ", stringifiedRoute)
 
-   // stringifiedRoute.split("/").slice(1).forEach(path=>{
     stringifiedRoute.split("/").forEach(path=>{
         console.log("// ",path)
-        rootObject = rootObject[path]
-        
+        rootObject = rootObject[path]     
     })
 
     return rootObject
 }
 
 /*
-return a closure which 
+implements dynamic routing based on the contents of the routesJSON object
+
+returns a closure which gets invoked when the corresponding route in express.js gets invoked
+the closure then resolves the method/user_type to invoke the matching function in the routesJSON object
 */
-const resolve = ( (_path, routesJSON) => {
-
-    const path = _path
-
+const routeResolver = ( (path, routesJSON) => {
     return function(req, res, next) {
 
         const key = req.query.key
@@ -64,34 +114,20 @@ const resolve = ( (_path, routesJSON) => {
 })
 
 module.exports = init = (app, router) => {
+  getStringRoutes(routes.JSON).forEach( (route) => {app.use(route.toLowerCase(), routeResolver(route, routes.JSON))})
+}
+ /*
 
-//getStringRoutes(routes.JSON)
-
-  // console.log(Object.keys(routes.JSON))
-
-  console.log("new")
-  getStringRoutes(routes.JSON).forEach( (route) => {
-    //console.log("route: ", '/' + route.toLowerCase())
-    //console.log("param: ", route.toLowerCase())
-    app.use('/'+ route.toLowerCase(), resolve(route, routes.JSON) )
-
-    //app.use(route.toLowerCase(), resolve(route, routes.JSON) )
-    })
-
-     /*
-    console.log("old")
+ console.log("old")
     Object.keys(routes.JSON).forEach( (route) => {
         console.log('/'+ route.toLowerCase())
         app.use('/'+ route.toLowerCase(), resolve(route) )
         })
- */
- 
-}
 
-function getStringRoutes(JSON){
+function getStringRoutesOLD(routesJSON){
 
     function f(rootStr, routeObjs){
-        const METHODS = [`POST`, `PATCH`, `GET`, `DELETE`]
+        const METHODS = [`POST`, `PATCH`, `GET`, `DELETE`, 'PUT']
         let addedMethod = false;
         let routes = [];
 
@@ -99,8 +135,8 @@ function getStringRoutes(JSON){
             if(!METHODS.includes(routeStr)){
                 routes = routes.concat ( f(`${rootStr}/${routeStr}`, routeObjs[routeStr]) );
             }else if(!addedMethod){
-                //routes.push(`/${rootStr}`);
-                routes.push(`${rootStr}`);
+                routes.push(`/${rootStr}`); // all a leading / to all routes
+                //routes.push(`${rootStr}`);
                 addedMethod = !addedMethod;
             }           
         }
@@ -109,20 +145,11 @@ function getStringRoutes(JSON){
 
     let stringifiedRoutes = []
 
-    Object.keys(JSON).forEach((rootStr)=>stringifiedRoutes = stringifiedRoutes.concat(f(rootStr, JSON[rootStr])))
+    Object.keys(routesJSON).forEach((rootStr)=>stringifiedRoutes = stringifiedRoutes.concat(f(rootStr, routesJSON[rootStr])))
 
-   
-   // console.log("stringifiedRoutes: ", getRootObject(stringifiedRoutes, JSON))
-
-  //  console.log("stringifiedRoutes: ", getRootObject(stringifiedRoutes, JSON))
-
+    console.log("stringifiedRoutes: ", stringifiedRoutes)
 
     return stringifiedRoutes
-
 }
 
-
-
-
-
-
+*/
